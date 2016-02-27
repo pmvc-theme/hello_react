86cc96ce91fec454db0c28cce6f147ec48816e5c
Hill <hill@kimo.com>
2016-02-27 14:59:33 +0000

    Sync with react 0.14.x

--- server.js
+++ server.js
@@ -2,13 +2,14 @@
 //process.argv.forEach(function (val, index, arr) {
 //  console.log(index + ': ' + val,arr);
 //});
+
+var ReactServer = require('react-dom/server');
 var args = process.argv.slice(2);
 var myJson = JSON.parse(args.join(''));
 var file = './assets/node.bundle.js'; 
 require(file);
 var MyApp = global.app;
-var React = global.React;
-var tmp = React.renderToString(MyApp(
+var tmp = ReactServer.renderToString(MyApp(
     myJson 
 ));
-console.log(tmp);
+console.log('<!--start-->'+tmp);
